e716dff76f7f3e02a9f15349f8554aa2dc1445b1
prateekdagur <prateek.dagur@npstx.com>
2025-07-20 11:10:07 +0530

    bid details

--- controllers/bidController.js
+++ controllers/bidController.js
@@ -100,10 +100,10 @@ export const getAuctionRanking = async (req, res) => {
     let bids;
     if (req.user.role === "Supplier") {
       // Only return this supplier's bids
-      bids = await Bid.find({ auction: auctionId, status: "Active", supplier: req.user.userId }).populate("lot supplier");;
+      bids = await Bid.find({ auction: auctionId, status: "Active", supplier: req.user.userId }).populate("lot supplier");
     } else {
       // For admin/EP, return all bids
-      bids = await Bid.find({ auction: auctionId, status: "Active" });
+      bids = await Bid.find({ auction: auctionId, status: "Active" }).populate("lot supplier");
     }
 
     // Calculate weighted score if costParams are set
